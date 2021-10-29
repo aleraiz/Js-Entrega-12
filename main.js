d26b2c6932118b49e8f47f6052b8c15a797582fc
aleraiz <alejandroraiz7@gmail.com>
2021-10-29 00:22:48 -0300

    commit

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 
 
+// Selectores con js
+// console.log(document.getElementById("zoom"));
+// console.log(document.getElementsByClassName("coder"));
+// console.log(document.getElementsByTagName("h1"));
 
-console.log(document.getElementById("zoom"));
-console.log(document.getElementsByClassName("coder"));
-console.log(document.getElementsByTagName("h1"));
 
-
-// Seleccionar con jquery - se selecciona igual que CSS
+// Seleccionar con jquery - se selecciona igual que CSS ( jQuery siempre nos trae un array, sea de un solo objero o varios)
 $("h1")
 $("#zoom")
 $(".coder")
@@ -43,8 +43,36 @@ document.getElementById("print2").innerHTML = `
 <p> DESDE INNER HTML </p>
 `
 
-//
+// agregar html con jquery
 
 $("#print3").append(`
 <p> DESDE append con jquery </p> 
 `)
+
+
+// Método ready() se emplea para detectar que el DOM está listo para usarse.
+// Cuando una página se está cargando, existe un tiempo de espera hasta que podamos manipular el DOM.
+$(document).ready(function() {
+    console.log("El DOM está listo");
+})
+
+
+// Para determinar cuando se cargaron todas las imágenes y otros recursos externos a la página usamos el evento load. El evento ready ocurre antes que load.
+window.addEventListener(`load`, function() {
+    console.log("Todos los elementos de la ventana están cargados");
+})
+
+// MÉTODO ON
+// Con el método on() podemos asignar eventos a elementos del DOM.
+// Es una opción al método addEventListener() de JS Vanilla cuando usamos jQuery
+
+// normal
+// document.getElementById("btn2").addEventListener("click", () => {
+//     console.log("click en enviar");
+// })
+
+
+//con jQuery
+$("btn3").click(()=> {
+    console.log("click en enviar con jQuery");
+})
